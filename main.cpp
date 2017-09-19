334fda586d212f809add2f3bbcf8245e63f61fd1
w00346734 <w00346734@huawei.com>
2017-09-19 15:37:57 +0800

    curl get string  result

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 #include <curl/curl.h>
-bool getPostToken(char *filename);
+std::string getPostToken();
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    getPostToken("/tmp/post.html");
+
+    std::string s_post_return;
+//    getPostToken();
+    std::cout << " s_post_return = "<<getPostToken() <<std::endl;
+//    Json::Reader reader;
     return 0;
 }
 
-bool getPostToken(char *filename)
+size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmemb, std::string *s)
+{
+    size_t newLength = size*nmemb;
+    size_t oldLength = s->size();
+    try
+    {
+        s->resize(oldLength + newLength);
+    }
+    catch(std::bad_alloc &e)
+    {
+        //handle memory problem
+        return 0;
+    }
+
+    std::copy((char*)contents,(char*)contents+newLength,s->begin()+oldLength);
+    return size*nmemb;
+}
+
+std::string getPostToken()
 {
     CURL *curl;
     CURLcode res;
@@ -22,16 +44,17 @@ bool getPostToken(char *filename)
 
 
 
-    FILE *fp;
-    if((fp =fopen(filename,"w"))==NULL)
-    {
-        return false;
-    }
+//    FILE *fp;
+//    if((fp =fopen(filename,"w"))==NULL)
+//    {
+//        return false;
+//    }
     curl = curl_easy_init();
+//    ;
+        std::string s_post_return;
 //    string strNew = " ";
     if(curl)
     {
-
         res= curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
         std::cout << "res CURLOPT_HTTPHEADER =" <<res<< std::endl;
 //        curl_easy_setopt(curl,CURLOPT_COOKIEFILE,"/tmp/cookie.txt");
@@ -45,15 +68,20 @@ bool getPostToken(char *filename)
         std::cout << "res CURLOPT_SSL_VERIFYPEER =" <<res<< std::endl;
 //        curl_easy_setopt(curl,CURLOPT_SSL_VERIFYHOST,0);
 //        curl_easy_setopt(curl,CURLOPT_URL,"http://code.huawei.com");
-        res = curl_easy_setopt(curl,CURLOPT_WRITEDATA,fp);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);
+        res = curl_easy_setopt(curl,CURLOPT_WRITEDATA,&s_post_return);
         std::cout << "res CURLOPT_WRITEDATA =" <<res<< std::endl;
+        curl_easy_setopt (curl, CURLOPT_VERBOSE, 1L); //remove this to disable verbose output
+
+
+
         res = curl_easy_perform(curl);
 
 
-        std::cout << "res curl_easy_perform=" <<res<< std::endl;
+        std::cout << "res curl_easy_perform=" <<res<<" and  return = "<< s_post_return << std::endl;
         curl_easy_cleanup(curl);
     }
-    fclose(fp);
-    return true;
+//    fclose(fp);
+    return s_post_return;
 
 }
